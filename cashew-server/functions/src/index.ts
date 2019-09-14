59ed97bfebdf3cb2505b0897d4177650fc83ef21
Kevin Trieu <dragn194@gmail.com>
2019-09-14 03:42:11 -0400

    Add function to update a station

--- cashew-server/functions/src/index.ts
+++ cashew-server/functions/src/index.ts
@@ -1,8 +1,30 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export interface Location {
+    x: number,
+    y: number,
+    floor: number
+}
+
+export interface Device {
+    mac_address: number,
+    signal_strength: number,
+}
+
+export interface Station {
+    id: number,
+	location: Location,
+	devices: Device[],
+	sound_level: number,
+	last_updated: number,
+}
+
+admin.initializeApp();
+
+export const stationUpdate = functions.https.onRequest(async (request, response) => {
+    let station = <Station>request.body;
+    let ref = admin.database().ref(`stations`).child(station.id.toString());
+    await ref.update(station);
+    response.json(ref.toJSON());
+})
